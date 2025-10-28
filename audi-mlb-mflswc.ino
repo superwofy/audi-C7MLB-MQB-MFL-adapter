31afb700d0a7548c9c2b244d2329172dbbe78822
Superwofy <adrian.bude@live.ie>
2025-10-28 21:33:49 +0000

    Master fixes

--- audi-mlb-mflswc.ino
+++ audi-mlb-mflswc.ino
@@ -25,46 +25,57 @@ uint8_t backlight_status_message[] = {0, 0x81, 0, 0, 0x71},                     
         buttons_status_message[] = {0x80, 0xF0, 0, 0, 0x21, 0, 0, 0, 0xDE},                                                         // First message upon connection
         steering_heater_status_message[] = {0x32, 0xFE, 0x14};                                                                      // 0C, button released
 
-uint8_t button_remap_array[] = {                                                                                                    // Label    Original value (MQB)
+uint8_t button_remap_array[] = {                                                                                                    // Label    MQB original value
         0x0,
         1,                                                                                                                          // Menu     (1)
         2,                                                                                                                          // Right    (2)
-        0x0, 0x0, 0x0,
+        3,                                                                                                                          // Left     (3)
+        0x0, 0x0,
         6,                                                                                                                          // Scroll   (6)
         7,                                                                                                                          // OK       (7)
-        0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0,
+        8,                                                                                                                          // Back     (8)     - MQB only
+        0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0,
         0x12,                                                                                                                       // Vol      (0x12)
         0x0, 0x0,
         0x15,                                                                                                                       // Next     (0x15)
         0x16,                                                                                                                       // Prev     (0x16)
         0x0, 0x0,
         0x19,                                                                                                                       // Voice    (0x19)
-        0x0, 0x0, 0x0, 0x0, 0x0, 0x0,
+        0x0,
+        0x1B,                                                                                                                       // Nav      (0x1B)
+        0x1C,                                                                                                                       // Phone    (0x1C)  - MQB only
+        0x0, 0x0, 0x0,
         0x20,                                                                                                                       // Mute     (0x20)
         0x21,                                                                                                                       // Joker*   (0x21)
-        0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0,
-        0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0,
-        0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0,
-        0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0,
-        0x0, 0x0, 0x0, 0x0, 0x0, 0x0,
-        0x71                                                                                                                        // RS mode  (0x71)
+        0x0,
+        0x23,                                                                                                                       // View     (0x23)  - MQB only
+        0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0,
+        0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0,
+        0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0,
+        0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0,
+        0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0,
+        0x71                                                                                                                        // RS mode  (0x71)  - MQB only
 };
 
 LinFrame master_frame = LinFrame(), slave_frame = LinFrame();
-bool e_message_initialized = false, ba_message_initialized = false, d_message_initialized = false;
+bool e_message_initialized = false, ba_message_initialized = false, d_message_initialized = false,
+     e_message_requested = false, ba_message_requested = false;
 
 void setup() {
 #if DEBUG_MODE
-  debug_serial.begin(19200);
+  debug_serial.begin(115200);
   while(!debug_serial);
-  debug_serial.print("Audi MQB to MLB LIN adapter for STM32 started. ");
-  debug_serial.print("Clock speed: ");
+  debug_serial.print("Audi MQB to MLB LIN adapter for STM32 started.");
+  debug_serial.print(" Clock speed: ");
   debug_serial.print(F_CPU / 1000000);
-  debug_serial.println(" MHz. ");
+  debug_serial.println(" MHz.");
 #endif
 
   pinMode(SW_TX_PIN, OUTPUT);
   car_lin.begin(19200);
+  send_lin_wakeup();
+
+  delay(200);                                                                                                                      // Needed for MQB buttons to initialize
 
   request_buttons_status_timer
   =request_heating_status_timer
@@ -80,31 +91,37 @@ void loop() {
 
   if (sw_lin.available()) {
     slave_comm_timer = millis();
-    byte b = sw_lin.read();
     byte n = slave_frame.num_bytes();
+    byte b = sw_lin.read();
 
-    // dump everything
-// #if DEBUG_MODE
-//     if (b == 0x55) {         
-//       debug_serial.println();
-//     }
-//     debug_serial.print(" ");
-//     debug_serial.print(b, HEX);
-//     return;
-// #endif
-
-    if (b == 0x55 && n > 1) {                                                                                                       // More than 2 bytes, i.e a data frame not a request.
-      slave_frame.pop_byte();
-      handle_slave_frame();
-      slave_frame.reset();
-    } else if (n == LinFrame::kMaxBytes) {
-      slave_frame.reset();
-    } else {
+    if (e_message_requested) {
+      if (n == 0) {
+        slave_frame.append_byte(0x8E);
+      }
       slave_frame.append_byte(b);
+      n = slave_frame.num_bytes();
+      if (n == 10) {
+        handle_slave_frame();
+        slave_frame.reset();
+        e_message_requested = false;
+      }
+    }
+
+    if (ba_message_requested) {
+      if (n == 0) {
+        slave_frame.append_byte(0xBA);
+      }
+      slave_frame.append_byte(b);
+      n = slave_frame.num_bytes();
+      if (n == 4) {
+        handle_slave_frame();
+        slave_frame.reset();
+        ba_message_requested = false;
+      }
     }
   }
 
-  if ((millis() - request_heating_status_timer) >= 250) {
+  if (((millis() - request_heating_status_timer) >= 120) && !ba_message_requested && !e_message_requested) {
     send_lin_break();                                                                                                               // Send LIN break
     sw_lin.write((uint8_t)0x55);                                                                                                    // Send sync byte (0x55)
     // delayMicroseconds(10);
@@ -112,12 +129,11 @@ void loop() {
     // sw_lin.flush();
 
     request_heating_status_timer = millis();
-    request_buttons_status_timer = millis();
+    ba_message_requested = true;
     return;
   }
 
-  if ((millis() - request_buttons_status_timer) >= 20) {
-
+  if (((millis() - request_buttons_status_timer) >= 20) && !e_message_requested && !ba_message_requested) {
     if ((millis() - backlight_status_message_timer) >= 100) {
       send_lin_break();
       sw_lin.write((uint8_t)0x55);
@@ -131,7 +147,7 @@ void loop() {
       backlight_status_message_timer = millis();
       sw_lin.flush();
     }
-    
+   
     send_lin_break();
     sw_lin.write((uint8_t)0x55);
     // delayMicroseconds(10);
@@ -139,6 +155,7 @@ void loop() {
     // sw_lin.flush();
 
     request_buttons_status_timer = millis();
+    e_message_requested = true;
   }
 
 
@@ -196,38 +213,51 @@ void loop() {
     }
   }
 
-  if (slave_comm_timer >= 30000) {
-    if (e_message_initialized || ba_message_initialized) {
+  if ((millis() - slave_comm_timer) >= 2000) {
 #if DEBUG_MODE
-      debug_serial.println("Timeout of slave RX - rebooting. ");
+    debug_serial.println("Timeout of slave RX. Resetting sw_lin.");
 #endif
-      NVIC_SystemReset();
-    }
+    send_lin_wakeup();
+    slave_comm_timer = millis();
+    request_heating_status_timer = millis();
+    request_buttons_status_timer = millis();
+    e_message_requested = false;
+    ba_message_requested = false;
+    e_message_initialized = false;
+    ba_message_initialized = false;
   }
-  if (master_comm_timer >= 30000) {
+  if ((millis() - master_comm_timer) >= 60000) {
     if (d_message_initialized) {
 #if DEBUG_MODE
-      debug_serial.println("Timeout of master RX - rebooting. ");
+      debug_serial.println("Timeout of master RX - rebooting.");
 #endif
       NVIC_SystemReset();
     }
   }
+}
+
+void send_lin_wakeup(void) {
+  // Send wakeup: Set TX low for 250..5000 µs
+  sw_lin.end();
+  digitalWrite(SW_TX_PIN, 0);
+  delayMicroseconds(500);
+  digitalWrite(SW_TX_PIN, 1);
+  sw_lin.begin(19200);
 }
 
 void send_lin_break(void) {
   // Send break: Set TX low for 13+ bit times
   // At 19200 baud, 1 bit = 52.09 µs, 13 bits = 677 µs
-  
   sw_lin.end();                                                                                                                     // Release USART control of TX pin
   digitalWrite(SW_TX_PIN, 0);                                                                                                       // Drive low for break
   delayMicroseconds(780);                                                                                                           // ~14.97 periods
   digitalWrite(SW_TX_PIN, 1);                                                                                                       // Release
+  delayMicroseconds(52);                                                                                                            // 1 Bit time
   sw_lin.begin(19200);                                                                                                              // Restart UART
-  // delayMicroseconds(10);
 }
 
 void handle_slave_frame(void) {
-  if (slave_frame.get_byte(slave_frame.num_bytes()) != check_frame_checksum(slave_frame)) {                                         // Validate checksum
+  if (slave_frame.get_byte(slave_frame.num_bytes() - 1) != check_frame_checksum(slave_frame)) {                                     // Validate checksum
     return;
   }
 
@@ -244,7 +274,7 @@ void handle_slave_frame(void) {
       buttons_status_message[3] = 1;                                                                                                // Change button direction to pressed
       bitWrite(buttons_status_message[7], 0, 0);                                                                                    // Force horn status to OFF
   #if DEBUG_BUTTON_PRESS
-      debug_serial.println("[ Drive Select ] ");
+      debug_serial.println("[ Drive Select ]");
   #endif
     } else {
       buttons_status_message[1] = button_remap_array[slave_frame.get_byte(2)];                                                      // Button ID
@@ -255,7 +285,7 @@ void handle_slave_frame(void) {
     buttons_status_message[3] = slave_frame.get_byte(4);
   #if DEBUG_BUTTON_PRESS
     if (bitRead(buttons_status_message[7], 0)) {
-      debug_serial.println("[ Horn ] ");
+      debug_serial.println("[ Horn ]");
     }
   #endif
 #endif
@@ -269,63 +299,78 @@ void handle_slave_frame(void) {
 
 #if DEBUG_BUTTON_PRESS
     if (buttons_status_message[1] == slave_frame.get_byte(2)) {                                                                     // Print only if the button is not remapped
-    switch (buttons_status_message[1]) {
+      switch (buttons_status_message[1]) {
         case 1:
-          debug_serial.println("[ Menu ] ");
+          debug_serial.println("[ Menu ]");
           break;
         case 2:
-          debug_serial.println("[ Right> ] ");
+          debug_serial.println("[ Right> ]");
           break;
         case 3:
-          debug_serial.println("[ <Left ] ");
+          debug_serial.println("[ <Left ]");
           break;
         case 6:
           if (buttons_status_message[3] == 0xF) {
-            debug_serial.println("[ Scroll- ] ");
+            debug_serial.println("[ Scroll- ]");
           } else if (buttons_status_message[3] == 1) {
-            debug_serial.println("[ Scroll+ ] ");
+            debug_serial.println("[ Scroll+ ]");
           }
           break;
         case 7:
-          debug_serial.println("[ OK ] ");
+          debug_serial.println("[ OK ]");
+          break;
+        case 8:
+          debug_serial.println("[ Back ]");
           break;
         case 0x12:
           if (buttons_status_message[3] == 0xF) {
-            debug_serial.println("[ Vol- ] ");
+            debug_serial.println("[ Vol- ]");
           } else if (buttons_status_message[3] == 1) {
-            debug_serial.println("[ Vol+ ] ");
+            debug_serial.println("[ Vol+ ]");
           }
           break;
         case 0x15:
-          debug_serial.println("[ Track>> ] ");
+          debug_serial.println("[ Track>> ]");
           break;
         case 0x16:
-          debug_serial.println("[ <<Track ] ");
+          debug_serial.println("[ <<Track ]");
           break;
         case 0x19:
-          debug_serial.println("[ Voice ] ");
+          debug_serial.println("[ Voice ]");
+          break;
+        case 0x1B:
+          debug_serial.println("[ Nav ]");
+          break;
+        case 0x1C:
+          debug_serial.println("[ Phone ]");
           break;
         case 0x20:
-          debug_serial.println("[ Mute ] ");
+          debug_serial.println("[ Mute ]");
           break;
         case 0x21:
-          debug_serial.println("[ Joker* ] ");
+          debug_serial.println("[ Joker* ]");
+          break;
+        case 0x23:
+          debug_serial.println("[ View ]");
+          break;
+        case 0x71:
+          debug_serial.println("[ RS Mode ]");
           break;
         default:
           break;
       }
     }
     if (bitRead(buttons_status_message[6], 0)) {
-      debug_serial.println("[ Paddle- ] ");
+      debug_serial.println("[ Paddle- ]");
     } else if (bitRead(buttons_status_message[6], 1)) {
-      debug_serial.println("[ Paddle+ ] ");
+      debug_serial.println("[ Paddle+ ]");
     }
 #endif
 
 #if DEBUG_MODE
     if (!e_message_initialized) {
       e_message_initialized = true;
-      debug_serial.println("Button status message initialized. ");
+      debug_serial.println("Button status message initialized.");
     }
 #else
       e_message_initialized = true;
@@ -346,14 +391,14 @@ void handle_slave_frame(void) {
     steering_heater_status_message[1] = slave_frame.get_byte(2);
 #if DEBUG_BUTTON_PRESS
     if (bitRead(steering_heater_status_message[1], 0)) {
-      debug_serial.println("[ SWHeat ] ");
+      debug_serial.println("[ SWHeat ]");
     }
 #endif
 
 #if DEBUG_MODE
     if (!ba_message_initialized) {
       ba_message_initialized = true;
-      debug_serial.println("Steering heater message initialized. ");
+      debug_serial.println("Steering heater message initialized.");
     }
 #else
     ba_message_initialized = true;
@@ -361,7 +406,7 @@ void handle_slave_frame(void) {
   }
 
 // #if DEBUG_MODE
-    // print_frame(slave_frame);
+//     print_frame(slave_frame);
 // #endif
 }
 
@@ -379,7 +424,7 @@ void handle_master_frame(void) {
 #if DEBUG_MODE
     if (!d_message_initialized) {
       d_message_initialized = true;
-      debug_serial.println("Backlight message initialized. ");
+      debug_serial.println("Backlight message initialized.");
     }
 #else
     d_message_initialized = true;
@@ -401,7 +446,7 @@ uint8_t calculate_lin2_checksum(uint8_t *data, uint8_t id, uint8_t size) {
 
 int check_frame_checksum(LinFrame frame) {
   int checksum = frame.get_byte(0);
-  for (uint8_t i = 1; i < frame.num_bytes(); i++) {
+  for (uint8_t i = 1; i < frame.num_bytes() - 1; i++) {
     checksum += frame.get_byte(i);
   }
   return 0xFF - (checksum % 0xFF);
@@ -410,7 +455,7 @@ int check_frame_checksum(LinFrame frame) {
 #if DEBUG_MODE
 void print_frame(LinFrame frame) {
   if (frame.num_bytes() > 1) {
-    for (uint8_t i = 0; i < frame.num_bytes() + 1; i++) {                                                                           // +1 to show checksum byte
+    for (uint8_t i = 0; i < frame.num_bytes(); i++) {
       if (i > frame.num_bytes()) {
         debug_serial.print("[");
       }
